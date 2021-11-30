6d5ed80067117e256bf4180eb8be878d7603a5f0
AlanLuna-00 <alan.luna.desarrollo@gmail.com>
2021-11-29 22:23:49 -0300

    navbar clase 3

---
+++ Clase-2/src/components/NavBar.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import logo from '../logo.svg'
+
+
+function NavBar() {
+    const stylesNavbar = {
+        backgroundColor: '#333',
+        display: 'flex',
+        justifyContent: 'space-between',
+    }
+    
+    const stylesLogoNav = {
+        width: '60px',
+        height: '60px',
+    }
+
+    const stylesUl = {
+        display: 'flex',
+        justifyContent: 'space-between',
+    }
+
+    const stylesLi = {
+        listStyle: 'none',
+    }
+
+    const stylesA = {
+        color: '#fff',
+        textDecoration: 'none',
+        padding: '10px',
+        fontSize: '20px',
+    }
+    return (
+        <div className="navbar"> 
+            <nav style={stylesNavbar}>
+                <img src={logo} style={stylesLogoNav} />
+                <ul style={stylesUl}>
+                    <li style={stylesLi}>
+                        <a style={stylesA} href="/">Home</a>
+                    </li>
+                    <li style={stylesLi}>
+                        <a style={stylesA} href="/about">About</a>
+                    </li>
+                    <li style={stylesLi}>
+                        <a style={stylesA} href="/contact">Contact</a>
+                    </li>
+                </ul>
+            </nav> 
+        </div>
+    )
+    
+}
+
+export default NavBar;
