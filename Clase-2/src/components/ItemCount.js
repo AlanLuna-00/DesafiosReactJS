634a0e517cfab709f98503a4ee8ce893a1600503
Alan Luna <alanluna00@MacBook-Air-de-Alan.local>
2021-12-08 20:37:20 -0300

    macbook commit

---
+++ Clase-2/src/components/ItemCount.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const ItemCount = () => {
+    const [number, setNumber] = useState(0)
+
+    const add = () => {
+        setNumber(number + 1)
+    }
+
+    const remove = () => {
+        number === 0 ? setNumber(0) : setNumber(number - 1)
+    }
+
+    return (
+        <div className="card shadow w-25 m-3">
+            <div className="row d-flex justify-content-between">
+                <button className="btn btn-outline-success col-4 mt-3 ms-3" onClick={add}>+</button>
+                <button className="btn btn-outline-danger col-4 mt-3 me-3" onClick={remove}>-</button>
+            </div>
+            <div className="row d-flex justify-content-center">
+                <h1 className="col-12 d-flex justify-content-center">{number}</h1>
+            </div>
+        </div>
+    )
+}
+
+export default ItemCount
